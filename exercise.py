79c925d159880230d377d9c4206e6266429d45fe
EY-X <e.yosefx128@gmail.com>
2019-08-02 00:51:44 -0400

    First Commit

---
+++ exercise.py
@@ -0,0 +1,35 @@
+classroom_seats = [
+    [None, "Pumpkin", None, None],
+    ["Socks", None, "Mimi", None],
+    ["Gismo", "Shadow", None, None],
+    ["Smokey","Toast","Pacha","Mau"]
+]
+
+
+# def seat_finder(classroom):
+    # open_seat = classroom
+for index_row, value in enumerate(classroom_seats):
+    index_row += 1
+    for x, y in enumerate(value):
+        x += 1
+        if y == None:
+            print(f'Row (index_row) seat {x} is available')
+            user_input = input('Would you like to sit here? (y/n)'
+
+            if user_input == 'y':
+
+
+
+
+
+ # if confirm == 'y': 
+				# 	name = input('Please enter your name.\n')
+				# 	seat_available[row_index-1 ][seat_index-1] = name 
+
+				# 	print(seat_available)
+				# 	exit() 
+
+
+
+
+               
